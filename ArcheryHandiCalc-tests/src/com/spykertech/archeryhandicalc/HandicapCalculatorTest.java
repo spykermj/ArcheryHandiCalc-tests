82deafce4f4a0e5252ef10cf9b2cceca57c30abd
spykermj <jeremy.spykerman@gmail.com>
2014-02-15 08:06:27 -0500

    root mean squared calculation tests

--- ArcheryHandiCalc-tests/src/com/spykertech/archeryhandicalc/HandicapCalculatorTest.java
+++ ArcheryHandiCalc-tests/src/com/spykertech/archeryhandicalc/HandicapCalculatorTest.java
@@ -8,4 +8,17 @@ public class HandicapCalculatorTest extends TestCase {
 		
 		assertEquals(oneHundredYardsAsMeters, HandicapCalculator.yardsToMeters(100.0), 0.00000001);
 	}
+	
+	public void testGetRMS() throws Exception {
+		double precision = 0.00001;
+		double oneHundredYardsHcapZero = 7.330498;
+		double oneHundredYardsHcapForty = 36.79815;
+		double ninetyMetersHcapZero = 7.21151;
+		double ninetyMetersHcapForty = 36.00009;
+		
+		assertEquals(oneHundredYardsHcapZero, HandicapCalculator.getRMS(0, 100, true), precision);
+		assertEquals(oneHundredYardsHcapForty, HandicapCalculator.getRMS(40, 100, true), precision);
+		assertEquals(ninetyMetersHcapZero, HandicapCalculator.getRMS(0, 90, false), precision);
+		assertEquals(ninetyMetersHcapForty, HandicapCalculator.getRMS(40, 90, false), precision);
+	}
 }
