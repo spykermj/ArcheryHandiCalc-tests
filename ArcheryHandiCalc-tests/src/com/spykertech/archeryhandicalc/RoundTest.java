15a65ba5a7c7aad8225636fca09a9c7494114bc0
spykermj <jeremy.spykerman@gmail.com>
2014-02-18 10:16:16 -0500

    Tests to look up score for handicap and handicap for score

--- ArcheryHandiCalc-tests/src/com/spykertech/archeryhandicalc/RoundTest.java
+++ ArcheryHandiCalc-tests/src/com/spykertech/archeryhandicalc/RoundTest.java
@@ -1,5 +1,7 @@
 package com.spykertech.archeryhandicalc;
 
+import java.util.List;
+
 import junit.framework.TestCase;
 
 public class RoundTest extends TestCase {
@@ -21,5 +23,51 @@ public class RoundTest extends TestCase {
 		Distance dist = new Distance(20.0, TargetFace.FITA60, ScoringStyle.TEN_ZONE, isMeters, arrows);
 		Round round = new Round("Portsmouth");
 		round.addDistance(dist);
+		List<Distance> list = round.getDistances();
+		assertEquals(1, list.size());
+		assertEquals(dist, list.get(0));
+	}
+	
+	public void testGetHandicapScore() throws Exception {
+		boolean isMeters = false;
+		int arrows = 60;
+		Distance dist = new Distance(20.0, TargetFace.FITA60, ScoringStyle.TEN_ZONE, isMeters, arrows);
+		Round portsmouth = new Round("Portsmouth");
+		portsmouth.addDistance(dist);
+		assertEquals(541, portsmouth.getHandicapScore(37));
+		Round york = new Round("York");
+		dist = new Distance(100, TargetFace.FITA122, ScoringStyle.IMPERIAL, false, 72);
+		york.addDistance(dist);
+		dist = new Distance(80, TargetFace.FITA122, ScoringStyle.IMPERIAL, false, 48);
+		york.addDistance(dist);
+		dist = new Distance(60, TargetFace.FITA122, ScoringStyle.IMPERIAL, false, 24);
+		york.addDistance(dist);
+		assertEquals(786, york.getHandicapScore(41));
+	}
+	
+	public void testGetHandicapTable() throws Exception {
+		int[] table;
+		Round york = new Round("York");
+		Distance dist = new Distance(100, TargetFace.FITA122, ScoringStyle.IMPERIAL, false, 72);
+		york.addDistance(dist);
+		dist = new Distance(80, TargetFace.FITA122, ScoringStyle.IMPERIAL, false, 48);
+		york.addDistance(dist);
+		dist = new Distance(60, TargetFace.FITA122, ScoringStyle.IMPERIAL, false, 24);
+		york.addDistance(dist);
+		table = york.getHandicapTable();
+		assertEquals(101, table.length);
+		assertEquals(698, table[44]);
+		assertEquals(1, table[100]);
+	}
+	
+	public void testLookupHandicap() throws Exception {
+		Round york = new Round("York");
+		Distance dist = new Distance(100, TargetFace.FITA122, ScoringStyle.IMPERIAL, false, 72);
+		york.addDistance(dist);
+		dist = new Distance(80, TargetFace.FITA122, ScoringStyle.IMPERIAL, false, 48);
+		york.addDistance(dist);
+		dist = new Distance(60, TargetFace.FITA122, ScoringStyle.IMPERIAL, false, 24);
+		york.addDistance(dist);
+		assertEquals(41, york.lookupHandicap(792));		
 	}
 }
