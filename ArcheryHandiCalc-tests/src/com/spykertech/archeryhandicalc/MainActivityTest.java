3ef5d88113e910cb62f4bb2e364fc91a56c462fc
Jeremy Spykerman <jeremy.spykerman@gmail.com>
2014-03-21 15:38:47 +0000

    Testing Vegas round handicap from the actual MainActivity class

---
+++ ArcheryHandiCalc-tests/src/com/spykertech/archeryhandicalc/MainActivityTest.java
@@ -0,0 +1,20 @@
+package com.spykertech.archeryhandicalc;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+
+public class MainActivityTest extends TestCase {
+	public void testBuildRoundList() throws Exception {
+		MainActivity activity = new MainActivity();
+		List<Round> list = activity.buildRoundList();
+		for(Round round : list) {			
+			if(round.toString() == "Vegas") {
+				int expectedHandicap = 47;
+				assertEquals(expectedHandicap, round.lookupHandicap(400));
+				expectedHandicap = 100;
+				assertEquals(expectedHandicap, round.lookupHandicap(8));
+			}
+		}
+	}
+}
